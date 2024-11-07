dacbcc4a8a1faca6a472fd7803b288e5e25c4467
Shreneken <shrnmallik@gmail.com>
2024-11-07 14:48:07 -0500

    added train test script

--- data_processing/utils.py
+++ data_processing/utils.py
@@ -5,6 +5,9 @@ import os
 import pandas as pd
 from shutil import copy
 import csv
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
 
 def jams_to_midi(jams_path, q=1):
     # q = 1: with pitch bend. q = 0: without pitch bend.
@@ -33,17 +36,22 @@ def jams_to_midi(jams_path, q=1):
             midi.instruments.append(midi_ch)
     return midi
 
-def process_guitarset(data_path, output_path):
-    audio_files = sorted([f for f in os.listdir(f'{data_path}/audio') if f.endswith('.wav')])
-    midi_files = sorted([f for f in os.listdir(f'{data_path}/midi') if f.endswith('.mid')])
 
-    data = {'audio_filename': audio_files, 'midi_filename': midi_files}
+def process_guitarset(data_path, output_path):
+    audio_files = sorted(
+        [f for f in os.listdir(f"{data_path}/audio") if f.endswith(".wav")]
+    )
+    midi_files = sorted(
+        [f for f in os.listdir(f"{data_path}/midi") if f.endswith(".mid")]
+    )
+
+    data = {"audio_filename": audio_files, "midi_filename": midi_files}
     df = pd.DataFrame(data)
     df.to_csv(output_path, index=False)
 
-    with open(output_path, 'w', newline='') as csvfile:
+    with open(output_path, "w", newline="") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(['audio_filename', 'midi_filename'])
+        writer.writerow(["audio_filename", "midi_filename"])
         for audio_file, midi_file in zip(audio_files, midi_files):
             writer.writerow([audio_file, midi_file])
 
@@ -65,3 +73,41 @@ def create_dataset(csv_path, data_path, output, audio_col, midi_col):
         copy(audio_path, output_path)
         output_path = os.path.join(output, "midi", midi_path.split("/")[-1])
         copy(midi_path, output_path)
+
+
+# Create train test split by calling this function on required subsets
+# Create 80-20 splits
+def create_train_test_split(input_path: str, train_path: str, test_path: str) -> None:
+    input_audio_path = os.path.join(input_path, "audio/")
+    input_midi_path = os.path.join(input_path, "midi/")
+
+    train_audio_path = os.path.join(train_path, "audio")
+    train_midi_path = os.path.join(train_path, "midi")
+
+    test_audio_path = os.path.join(test_path, "audio")
+    test_midi_path = os.path.join(test_path, "midi")
+
+    input_files = zip(
+        sorted(os.listdir(input_audio_path)), sorted(os.listdir(input_midi_path))
+    )
+    train_files, test_files = train_test_split(input_files, 0.2, 0.8, shuffle=True)
+    for train_audio_file, train_midi_file in train_files:
+        copy(
+            os.path.join(input_audio_path, train_audio_file),
+            os.path.join(train_audio_path, train_audio_file),
+        )
+        copy(
+            os.path.join(input_midi_path, train_midi_file),
+            os.path.join(train_midi_path, train_midi_file),
+        )
+
+    for test_audio_file, test_midi_file in test_files:
+
+        copy(
+            os.path.join(input_audio_path, test_audio_file),
+            os.path.join(test_audio_path, test_audio_file),
+        )
+        copy(
+            os.path.join(input_midi_path, test_midi_file),
+            os.path.join(test_midi_path, test_midi_file),
+        )
