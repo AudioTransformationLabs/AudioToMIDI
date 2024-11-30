ffc490204f4bbfd716c6589f7925b661d0f36488
Shreyan Mallik <shrnmallik@gmail.com>
2024-11-30 17:16:54 -0500

    added gitignore stuff

--- data_processing/train.py
+++ data_processing/train.py
@@ -1,6 +1,7 @@
 import os
 import torch
 import json
+from torch._prims_common import device_or_default
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
@@ -23,17 +24,19 @@ from .evaluate import evaluate_model, load_model
 from .model import AudioToMidiCNN
 from .transformer import Transformer
 
+
 def load_checkpoint_if_exists(model_path, optimizer_path, params, device):
     if not os.path.exists(model_path) or not os.path.exists(optimizer_path):
-        model = AudioToMidiCNN(dropout=params['dropout']).to(device)
-        optimizer = optim.Adam(model.parameters(), lr=params['learning_rate'])
+        model = AudioToMidiCNN(dropout=params["dropout"]).to(device)
+        optimizer = optim.Adam(model.parameters(), lr=params["learning_rate"])
         return model, optimizer
     else:
         model = load_model(model_path, device)
-        optimizer = optim.Adam(model.parameters(), lr=params['learning_rate'])
+        optimizer = optim.Adam(model.parameters(), lr=params["learning_rate"])
         optimizer.load_state_dict(torch.load(optimizer_path, weights_only=True))
         return model, optimizer
-    
+
+
 def load_dataset_if_exists(train_data_path, test_data_path):
     if not os.path.exists(train_data_path) or not os.path.exists(test_data_path):
         transform = (
@@ -51,12 +54,27 @@ def load_dataset_if_exists(train_data_path, test_data_path):
         test_dataset = torch.load(test_data_path)
         return train_dataset, test_dataset
 
-def train_model(model, learning_rate, dropout, dataloader, optimizer, device, start_epoch=0, epochs=NUM_EPOCHS, threshold=0.5):
-    print(f'Learning Rate: {learning_rate}, Dropout: {dropout}')
+
+def train_model(
+    model,
+    learning_rate,
+    dropout,
+    dataloader,
+    optimizer,
+    device,
+    start_epoch=0,
+    epochs=NUM_EPOCHS,
+    threshold=0.5,
+):
+    print(f"Learning Rate: {learning_rate}, Dropout: {dropout}")
     model.train()
     for epoch in range(start_epoch, start_epoch + epochs):
         curr_loss = 0.0
-        for audio, midi in tqdm(dataloader, desc=f"Epoch {epoch + 1} / {start_epoch + epochs}", total=len(dataloader)):
+        for audio, midi in tqdm(
+            dataloader,
+            desc=f"Epoch {epoch + 1} / {start_epoch + epochs}",
+            total=len(dataloader),
+        ):
             audio, midi = audio.to(device), midi.to(device)
             optimizer.zero_grad()
 
@@ -75,9 +93,12 @@ def train_model(model, learning_rate, dropout, dataloader, optimizer, device, st
 
             curr_loss += loss.item()
 
-        print(f"Epoch {epoch + 1}/{start_epoch + epochs}, Loss: {curr_loss / len(dataloader)}")
+        print(
+            f"Epoch {epoch + 1}/{start_epoch + epochs}, Loss: {curr_loss / len(dataloader)}"
+        )
 
     return model
+
 
 if __name__ == "__main__":
     transform = (
@@ -85,10 +106,16 @@ if __name__ == "__main__":
         if FEATURE_TYPE == "mel_spec"
         else Transformer.mfcc_transform()
     )
-    device = torch.device("mps" if torch.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device(
+        "mps"
+        if torch.mps.is_available()
+        else "cuda" if torch.cuda.is_available() else "cpu"
+    )
     print(f"Using {device} to run model training")
 
-    train_dataset, test_dataset = load_dataset_if_exists("data/train_dataset.pth", "data/test_dataset.pth")
+    train_dataset, test_dataset = load_dataset_if_exists(
+        "data/train_dataset.pth", "data/test_dataset.pth"
+    )
     train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
@@ -102,21 +129,34 @@ if __name__ == "__main__":
             model, optimizer = load_checkpoint_if_exists(
                 f"models/{MODEL_NAME}_melspec_LR={learning_rate}_DROPOUT={dropout}.pth",
                 f"models/{MODEL_NAME}_melspec_LR={learning_rate}_DROPOUT={dropout}_optimizer.pth",
-                { "learning_rate": learning_rate, "dropout": dropout },
-                device
+                {"learning_rate": learning_rate, "dropout": dropout},
+                device,
             )
 
             trained_model = train_model(
-                model, learning_rate, dropout, train_loader, optimizer, start_epoch=100, epochs=NUM_EPOCHS
+                model,
+                learning_rate,
+                dropout,
+                train_loader,
+                optimizer,
+                start_epoch=100,
+                epochs=NUM_EPOCHS,
+                device=device,
             )
-            torch.save(trained_model.state_dict(), f"models/{MODEL_NAME}_melspec_LR={learning_rate}_DROPOUT={dropout}.pth")
-            torch.save(optimizer.state_dict(), f"models/{MODEL_NAME}_melspec_LR={learning_rate}_DROPOUT={dropout}_optimizer.pth")
+            torch.save(
+                trained_model.state_dict(),
+                f"models/{MODEL_NAME}_melspec_LR={learning_rate}_DROPOUT={dropout}.pth",
+            )
+            torch.save(
+                optimizer.state_dict(),
+                f"models/{MODEL_NAME}_melspec_LR={learning_rate}_DROPOUT={dropout}_optimizer.pth",
+            )
 
             results = evaluate_model(trained_model, test_loader, device)
             print(f"Results for LR={learning_rate}, Dropout={dropout}: {results}")
 
-            if results['f1_score'] >= best_f1:
-                best_f1 = results['f1_score']
+            if results["f1_score"] >= best_f1:
+                best_f1 = results["f1_score"]
                 best_model = {
                     "learning_rate": learning_rate,
                     "dropout": dropout,
